9b0d4c48cdfd703961a30f5baee1f1df2c00c33e
ansango <anibalsantosgo@gmail.com>
2023-03-03 18:56:23 +0100

    feat(config): initial config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMDX = require("@next/mdx")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+    mdxRs: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig);
